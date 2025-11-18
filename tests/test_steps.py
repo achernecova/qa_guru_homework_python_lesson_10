10a6e6fcd1ec04d253716a3b10cca1372afbe671
Alexandra Chernetsova <achernecova1904@gmail.com>
2025-11-18 23:04:10 +0300

    init commit 2

--- tests/test_steps.py
+++ tests/test_steps.py
@@ -1,5 +1,6 @@
 from time import sleep
 
+import allure
 from selene import browser, by, be
 from selene.support.shared.jquery_style import s
 
@@ -7,19 +8,55 @@ from selene.support.shared.jquery_style import s
 # Пишем тест на гитхаб - eroshenkoam/allure-example - проваливаемся в первую репу.
 # Ищем issues - и его там нет. Что делать?... Возьмем id = pull-requests-tab
 
-def test_github():
+def test_dynamic_github():
+
+    with allure.step('Открываем главную страницу'):
+        browser.open("https://github.com")
+
+    with allure.step('Ищем репозиторий'):
+        s('.header-search-button').click()
+    # sleep(60)
+        s('.FormControl-input').send_keys('eroshenkoam/allure-example')
+        sleep(5)
+        s('.FormControl-input').submit()
+
+    sleep(5)
+    with allure.step('Переходим по ссылке репозитория'):
+        s(by.link_text('eroshenkoam/allure-example')).click()
+    sleep(5)
+    with allure.step('Кликаем по табу requests'):
+        s('#pull-requests-tab').click()
+    sleep(5)
+    with allure.step('Проверяем, что есть Pull request с номером #91'):
+        s(by.partial_text('#90')).should(be.visible)
+
+def test_decorator_steps():
+    open_main_page()
+    search_for_repositiry('eroshenkoam/allure-example')
+    go_to_repository('eroshenkoam/allure-example')
+    open_tab()
+    should_see_requests_with_number("91")
+
+@allure.step('Открываем главную страницу')
+def open_main_page():
     browser.open("https://github.com")
 
-    s('.header-search-button').click()
+@allure.step('Ищем репозиторий {repo}')
+def search_for_repositiry(repo):
+        s('.header-search-button').click()
     # sleep(60)
-    s('.FormControl-input').send_keys('eroshenkoam/allure-example')
-    sleep(5)
-    s('.FormControl-input').submit()
+        s('.FormControl-input').send_keys(repo)
+        sleep(5)
+        s('.FormControl-input').submit()
 
-    sleep(5)
-    s(by.link_text('eroshenkoam/allure-example')).click()
-    sleep(5)
+@allure.step('Переходим по ссылке репозитория {repo}')
+def go_to_repository(repo):
+    s(by.link_text(repo)).click()
+
+@allure.step('Кликаем по табу requests')
+def open_tab():
     s('#pull-requests-tab').click()
-    sleep(5)
-    s(by.partial_text('#90')).should(be.visible)
 
+@allure.step('Проверяем, что есть Pull request с номером {number}')
+def should_see_requests_with_number(number):
+    s(by.partial_text('#' + number)).should(be.visible)
