904ca86f9ef7a8358dd300f328e35a70d9b1c7d5
Alexandra Chernetsova <achernecova1904@gmail.com>
2025-11-19 17:51:41 +0300

    Refactor tests in file test_selene.py , test_steps.py.

--- tests/test_selene.py
+++ tests/test_selene.py
@@ -1,4 +1,4 @@
-from selene import be, browser, by
+from selene import be, browser, by, have
 from selene.support.shared.jquery_style import s
 
 # Пишем тест на гитхаб - eroshenkoam/allure-example - проваливаемся в первую репу.
@@ -9,14 +9,9 @@ def test_github():
     browser.open("https://github.com")
 
     s(".header-search-button").click()
-    # sleep(60)
-    s(".FormControl-input").send_keys("eroshenkoam/allure-example")
-    # sleep(5)
+    s(".FormControl-input").send_keys("eroshenkoam/allure-playwright-example")
     s(".FormControl-input").submit()
+    s(by.link_text("eroshenkoam/allure-playwright-example")).click()
+    s("#issues-tab").click()
+    s("[href='/eroshenkoam/allure-playwright-example/issues/1']").should(have.exact_text("Не работает переход по табу Issues"))
 
-    # sleep(5)
-    s(by.link_text("eroshenkoam/allure-example")).click()
-    # sleep(5)
-    s("#pull-requests-tab").click()
-    # sleep(5)
-    s(by.partial_text("#90")).should(be.visible)
--- tests/test_steps.py
+++ tests/test_steps.py
@@ -1,43 +1,41 @@
 from time import sleep
 
-from selene import be, browser, by
+import allure
+from selene import browser, by, have
 from selene.support.shared.jquery_style import s
 
-import allure
 
 # Пишем тест на гитхаб - eroshenkoam/allure-example - проваливаемся в первую репу.
 # Ищем issues - и его там нет. Что делать?... Возьмем id = pull-requests-tab
 
-
+# Взяла другую репу, где есть issues
 def test_dynamic_github():
-
     with allure.step("Открываем главную страницу"):
         browser.open("https://github.com")
 
     with allure.step("Ищем репозиторий"):
         s(".header-search-button").click()
-        # sleep(60)
-        s(".FormControl-input").send_keys("eroshenkoam/allure-example")
-        sleep(5)
+        s(".FormControl-input").send_keys("eroshenkoam/allure-playwright-example")
         s(".FormControl-input").submit()
 
-    sleep(5)
     with allure.step("Переходим по ссылке репозитория"):
-        s(by.link_text("eroshenkoam/allure-example")).click()
-    sleep(5)
-    with allure.step("Кликаем по табу requests"):
-        s("#pull-requests-tab").click()
-    sleep(5)
-    with allure.step("Проверяем, что есть Pull request с номером #91"):
-        s(by.partial_text("#90")).should(be.visible)
+        s(by.link_text("eroshenkoam/allure-playwright-example")).click()
+
+    with allure.step("Кликаем по табу Issues"):
+        s("#issues-tab").click()
+
+    with allure.step("Проверяем, что есть Issues с названием  Не работает переход по табу Issues"):
+        s("[href='/eroshenkoam/allure-playwright-example/issues/1']").should(
+            have.exact_text("Не работает переход по табу Issues"))
 
 
+# Issue скрыто для данного проекта
 def test_decorator_steps():
     open_main_page()
     search_for_repositiry("eroshenkoam/allure-example")
     go_to_repository("eroshenkoam/allure-example")
     open_tab()
-    should_see_requests_with_number("91")
+    should_see_requests_with_text("Не работает переход по табу Issues")
 
 
 @allure.step("Открываем главную страницу")
@@ -48,9 +46,7 @@ def open_main_page():
 @allure.step("Ищем репозиторий {repo}")
 def search_for_repositiry(repo):
     s(".header-search-button").click()
-    # sleep(60)
     s(".FormControl-input").send_keys(repo)
-    sleep(5)
     s(".FormControl-input").submit()
 
 
@@ -64,6 +60,7 @@ def open_tab():
     s("#pull-requests-tab").click()
 
 
-@allure.step("Проверяем, что есть Pull request с номером {number}")
-def should_see_requests_with_number(number):
-    s(by.partial_text("#" + number)).should(be.visible)
+@allure.step("Проверяем, что есть Pull request с текстом {text}")
+def should_see_requests_with_text(text):
+    s("[href='/eroshenkoam/allure-playwright-example/issues/1']").should(have.exact_text(
+        {text}))
